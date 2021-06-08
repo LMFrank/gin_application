87a4b04128ded651a37deda468664c5e6aa4f053
LMFrank <njuxushuchi@163.com>
2021-06-08 22:22:44 +0800

    使用jwt-go完成了接口的身份校验

--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"github.com/LMFrank/gin_project/middleware/jwt"
 	"github.com/LMFrank/gin_project/pkg/setting"
+	"github.com/LMFrank/gin_project/routers/api"
 	v1 "github.com/LMFrank/gin_project/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -12,27 +14,32 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 
-	// 获取标签列表
-	apiv1.GET("/tags", v1.GetTags)
-	// 新建标签
-	apiv1.POST("/tags", v1.AddTag)
-	// 更新指定标签
-	apiv1.PUT("/tags/:id", v1.EditTag)
-	// 删除指定标签
-	apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	{
+		// 获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		// 新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		// 更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		// 删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 
-	// 获取文章列表
-	apiv1.GET("/articles", v1.GetArticles)
-	// 获取指定文章
-	apiv1.GET("/articles/:id", v1.GetArticle)
-	// 新建文章
-	apiv1.POST("/articles", v1.AddArticle)
-	// 更新指定文章
-	apiv1.PUT("/articles/:id", v1.EditArticle)
-	// 删除指定文章
-	apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+		// 获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		// 获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		// 新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		// 更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		// 删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+	}
 
 	return r
 }
