eaa11c2f724493495e423221a83ad2efcf0bc125
LMFrank <njuxushuchi@163.com>
2021-06-17 10:49:22 +0800

    更新了重启功能，可以对http服务进行热更新

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/LMFrank/gin_project/pkg/setting"
 	"github.com/LMFrank/gin_project/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -18,5 +23,40 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
+	/*
+		endless.DefaultReadTimeOut = setting.ReadTimeout
+		endless.DefaultWriteTimeOut = setting.WriteTimeout
+		endless.DefaultMaxHeaderBytes = 1 << 20
+		endpoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+		server := endless.NewServer(endpoint, routers.InitRouter())
+		server.BeforeBegin = func(add string) {
+			log.Printf("Actual pid is %d", syscall.Getpid())
+		}
+
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Printf("Server err: %v", err)
+		}
+	*/
 }
